8ca61b4f136b62e5cd15bbbead1d54bebe5923ac
workingdog <ringowathelet@gmail.com>
2023-10-02 12:22:30 +0900

    Initial Commit

---
+++ MapPoly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapPoly
+//
+//  Created by Ringo Wathelet on 2023/10/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapPoly/MapPolyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPolyApp.swift
+//  MapPoly
+//
+//  Created by Ringo Wathelet on 2023/10/02.
+//
+
+import SwiftUI
+
+@main
+struct MapPolyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
